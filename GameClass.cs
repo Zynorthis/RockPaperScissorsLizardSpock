a408aba1b5985d3466a0f3ee3014a8b24fb78028
Jacob Taylor <jacobtaylor727@outlook.com>
2018-11-21 15:53:21 -0600

    First commit to get things setup

---
+++ GameClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsLizardSpock
+{
+    class GameClass
+    {
+        // decide round winners
+        // decide overall winner
+        // select amount of rounds
+    }
+}
